e500299e4181e1885ac9cc5275ebfa6f511f0b4d
iyo-bemoore <imadyoussoufi@gmail.com>
2022-06-09 16:09:47 +0200

    init

---
+++ Part 2/JoiningStrings.java
@@ -0,0 +1,11 @@
+public class JoiningStrings {
+    public static void main(String[] args) {
+        String sentence = "His name is: ";
+        String placeHolder = "Harry";
+        int points = 50;
+        String anouncement = " points for Gryfeenod";
+        System.out.println(sentence + placeHolder);
+        System.out.println(points + anouncement);
+
+    }
+}
