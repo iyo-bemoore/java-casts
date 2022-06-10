25c50ed195e9aa6df99a8f4637a7a746b3ae982f
iyo-bemoore <imadyoussoufi@gmail.com>
2022-06-10 15:52:07 +0200

    math

---
+++ Part 2/WholeNumbers.java
@@ -0,0 +1,10 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        int peopleOnThePlace = 653456568;
+        long worldPopulation = 7000000000L; // Add L to confirm long intention. otherwise won't compile.
+
+        System.out.println("there are " + peopleOnThePlace + " in the place");
+        System.out.println("there are " + worldPopulation + " on the planet");
+
+    }
+}
