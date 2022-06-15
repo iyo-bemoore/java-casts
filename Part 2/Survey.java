09bee917dcdacf09cb3f677b9c2cffcbf008cd3f
iyo-bemoore <imadyoussoufi@gmail.com>
2022-06-15 14:47:03 +0200

    final survey

--- Part 2/Survey.java
+++ Part 2/Survey.java
@@ -5,13 +5,13 @@ public class Survey {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in).useLocale(Locale.ENGLISH);
         System.out.println("Welcome to food & beverage survey");
-        System.out.println("What is your name?");
+        System.out.println("\nWhat is your name?");
         String name = scan.nextLine();
         System.out.println("Hello "+name);
-        System.out.println("How old are you?");
+        System.out.println("\nHow old are you?");
         int age = scan.nextInt();
         System.out.println("Thank you "+name+" We will shortly ask you a couple of questions regarding your consumption habits.");
-        System.out.println("Write YES to proceed or NO to exit");
+        System.out.println("\nWrite YES to proceed or NO to exit");
         scan.nextLine();
         String  approval = scan.nextLine();
         System.out.println("Approval is " +approval);
@@ -21,12 +21,12 @@ public class Survey {
           System.exit(0);
           return;
         }        
-        System.out.println("How much do you spend on coffee on a weekly basis?");
+        System.out.println("\nHow much do you spend on coffee on a weekly basis?");
         double coffeeSpendings = scan.nextDouble();
-        System.out.println("How much do you spend on Fast food on weekly basis?");
+        System.out.println("\nHow much do you spend on Fast food on weekly basis?");
         double fastFoodSpendings = scan.nextDouble();
-        System.out.println("Thank you for your participation");
-        System.out.println("--------- Summary ---------- ");
+        System.out.println("\nThank you for your participation!");
+        System.out.println("\n--------- Summary ---------- ");
         System.out.println("Participant name: "+name);
         System.out.println("Participant age: "+age+" years old.");
         System.out.println("Participant Coffee spendings: "+coffeeSpendings+"$");
